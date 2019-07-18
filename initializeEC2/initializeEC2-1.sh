fefce2a359f6c6c195725d9937ca9b3a65d65296
Junho Kim-Lee <52838674+JunhoKimLee@users.noreply.github.com>
2019-07-18 17:21:51 -0400

    Add files via upload

---
+++ initializeEC2/initializeEC2-1.sh
@@ -0,0 +1,19 @@
+echo "Setting time to EST."
+sudo cp /usr/share/zoneinfo/America/New_York /etc/localtime
+
+echo "Implementing shutdown failsafes via Crontab."
+sudo crontab -l -u root | echo "* 18 * * 5 /sbin/shutdown -h now" | sudo crontab -u root -
+echo "But now there is!"
+
+echo "Installing wget, zip, unzip, file, bzip2, nano."
+sudo yum install wget zip unzip file bzip2 nano
+
+echo "Downloading Anaconda."
+wget https://repo.anaconda.com/archive/Anaconda3-2019.03-Linux-x86_64.sh
+
+echo "Installing Anaconda."
+bash Anaconda3-2019.03-Linux-x86_64.sh
+rm Anaconda3-2019.03-Linux-x86_64.sh
+
+echo "Success! Anaconda installed."
+echo "Close and reopen this terminal window for changes to take effect. Then execute the second installer."
---
+++ initializeEC2/initializeEC2-2.sh
@@ -0,0 +1,23 @@
+#echo "Updating Anaconda to latest version."
+#conda update -n root conda
+#conda update --all
+#conda --version
+
+#echo "Installing GDAL."
+#conda install gdal
+
+echo "Downloading some of Junho's useful scripts..."
+echo "Downloading isRunning.sh - A script that will tell you if a script is running in the background."
+wget https://github.com/JunhoKimLee/Goddard/blob/master/isRunning.sh
+echo "Downloading isShutdown.sh - A script that will tell you if a shutdown has been scheduled."
+wget https://github.com/JunhoKimLee/Goddard/blob/master/isShutdown.sh
+echo "Downloading scheduledShutdown.sh - A script that will let you schedule a future shutdown."
+wget https://github.com/JunhoKimLee/Goddard/blob/master/scheduledShutdown.sh
+
+echo "Organizing some stuff..."
+mkdir Useful_Scripts
+mv isRunning.sh Useful_Scripts/isRunning.sh
+mv isShutdown.sh Useful_Scripts/isShutdown.sh
+mv scheduledShutdown.sh Useful_Scripts/scheduledShutdown.sh
+
+echo "Success! This EC2 instance has been initialized."
